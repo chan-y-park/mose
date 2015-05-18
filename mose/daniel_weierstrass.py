1) Implemented automatic rotation of x plane. 2) Included latest fixes by Dniel to weierstrass module. 3) Successfully interfaces weierstrass module with main part of the code

mose/daniel_weierstrass.py
@@ -403,8 +403,7 @@ def invert_monodromy(MM):
         
       
 def monodromy_at_point_via_path(init_root,d,f,g,path_data,\
-                                 ts_ipath=1000,ts_mon=1000,option=None,\
-                            rotation_recursion_level=0, rotation_n=10):
+                                 ts_ipath=1000,ts_mon=1000,option=None):
     """
     monodromy_at_point_via_path(init_root,d,f,g,path_data,ts_ipath,ts_mon)
     
@@ -434,86 +433,34 @@ def monodromy_at_point_via_path(init_root,d,f,g,path_data,\
     The monodromy matrix around the point "d" via
     the path constructed using path_data
     """
-
-    def real_part(x,y):
-            if x.real>y.real:
-                return 1
-            else: return -1
-
     total_path = construct_path(d,path_data)
     initial_path = total_path[:3]
     circ_path = total_path[2:]
     MM = np.matrix([[1,0],[0,1]])
-
-    original_init_root = init_root
-
-    rot_rec_lvl = rotation_recursion_level
-    if rot_rec_lvl>0 and rot_rec_lvl<rotation_n:
-        print "I have rotated the x-plane %s times so far." % rot_rec_lvl
-        print "f = \n%s\n\ng = \n%s\n" % (f, g)
-    elif rot_rec_lvl>=rotation_n:
-        raise ValueError('Rotated a full 2pi angle, and couldnt '+\
-            'compute the braiding.')
-
-    control_var = 'fine'
     
     segments=len(initial_path)-1
     for s in range(segments):
         ev_result=evolve_to_get_braiding(init_root,f,g,\
                                         [initial_path[s],initial_path[s+1]],\
                                         ts_ipath)
-        if ev_result == 'rotate':
-            control_var = 'rotate'
-            break
-        
-        else:
-            if option == 'p':
-                print("Initialization path "+str(s)+": "+str(ev_result[0]))
-            MM = np.dot(MM,monodromy(ev_result[0]))
-            init_root = ev_result[1]
-        
+        if option == 'p':
+            print("Initialization path "+str(s)+": "+str(ev_result[0]))
+        MM = np.dot(MM,monodromy(ev_result[0]))
+        init_root = ev_result[1]
+    
     init_MM = MM
     
-    if control_var == 'fine':   #proceed with circular part of the path
-        segments=len(circ_path)-1
-        for s in range(segments):
-            ev_result=evolve_to_get_braiding(init_root,f,g,\
-                                            [circ_path[s],circ_path[s+1]],\
-                                            ts_mon)
-            if ev_result == 'rotate':
-                control_var = 'rotate'
-                break
-
-            else:
-                if option == 'p':
-                    print("Encircling path "+str(s)+":"+str(ev_result[0]))
-                MM = np.dot(MM,monodromy(ev_result[0]))
-                init_root = ev_result[1]
-        
-    if control_var == 'fine':
-        return np.dot(MM,invert_monodromy(init_MM))
-
-    elif control_var == 'rotate':
-        print "\n\
-            *************************************\n\
-            Having trouble tracking root braiding\n\
-                    Will rotate the x-plane      \n\
-            *************************************\n"
-        zeta = np.exp(2*np.pi*1j/rotation_n)
-        r_f = f / (zeta**2) #[x / (zeta**2) for x in f]
-        r_g = g / (zeta**3) #[x / (zeta**3) for x in f]
-        # print f
-        # print r_f
-        # print init_root
-        # print type(init_root)
-        r_init_root = sorted([(x / zeta) for x in original_init_root], cmp=real_part)
-        # print r_init_root
-        # print type(r_init_root)
-        
-        return monodromy_at_point_via_path(r_init_root, d, r_f, r_g, path_data, \
-                            ts_ipath=ts_ipath,ts_mon=ts_mon,option=option,\
-                            rotation_recursion_level=rot_rec_lvl+1)
-
+    segments=len(circ_path)-1
+    for s in range(segments):
+        ev_result=evolve_to_get_braiding(init_root,f,g,\
+                                        [circ_path[s],circ_path[s+1]],\
+                                        ts_mon)
+        if option == 'p':
+            print("Encircling path "+str(s)+":"+str(ev_result[0]))
+        MM = np.dot(MM,monodromy(ev_result[0]))
+        init_root = ev_result[1]
+    
+    return np.dot(MM,invert_monodromy(init_MM))
 
             
 def monodromy_at_point_wmodel(n,wmodel,ts_ipath=1000,ts_mon=1000,option=None):
@@ -730,12 +677,8 @@ def evolve_to_get_braiding(init_data,f,g,start_end,timesteps=1000):
                     min_diff = np.absolute(a-b)
             max_diff_cons = max(max_diff_cons,min_diff)
         if is_degenerate(sorted_rts):
-            # raise ValueError('Roots not being properly distinguished: '+\
-            #                    'timesteps too sparse.')
-            print '\nsort_roots:: Roots not being properly distinguished: '+\
-                               'timesteps too sparse. '+ \
-                               '\n\nrts:\n%s\n\nlast_rts:\n%s\n\nsorted_rts\n%s' % (rts,last_rts,sorted_rts)
-            return ['rotate', 'rotate']
+            raise ValueError('Roots not being properly distinguished: '+\
+                               'timesteps too sparse.')
         else:            
             return [sorted_rts,max_diff_cons]    
 #        return [sorted_rts, max_diff_cons]
@@ -792,9 +735,7 @@ def evolve_to_get_braiding(init_data,f,g,start_end,timesteps=1000):
                     raise ValueError('Cannot distinguish y and Y.')
                 return 'y'
         else:
-            # raise ValueError('Timesteps too small to extract braiding.')
-            print '\nbraiding_action:: Timesteps too small to extract braiding.'
-            return 'rotate'
+            raise ValueError('Timesteps too small to extract braiding.')
                 
     def minimum_diff_rts(rts):
         diffs = []
@@ -818,9 +759,7 @@ def evolve_to_get_braiding(init_data,f,g,start_end,timesteps=1000):
     last_roots = init_data
     time = np.linspace(0,1.,timesteps)
     G = []
-    
-    control_var = 'fine'
-
+        
     #solve differential equation
     for t in time[1:]:
         poly = np.poly1d([1,0,f(start+t*vector),g(start+t*vector)])
@@ -828,42 +767,25 @@ def evolve_to_get_braiding(init_data,f,g,start_end,timesteps=1000):
         #print 'Time : '+str(t)
         #print 'Roots : '+str(roots)
         roots, cons = sort_roots(roots,last_roots)
-        # if is_degenerate(roots):
-        #     print 'Last roots: '+str(last_roots)
-        #     print 'Current roots:'+str(roots)
-        #     print 'Path data:'+str(start)+' to '+str(end)
-        #     print 'Location'+str(start+t*vector)
-        #     raise ValueError('Roots not being properly distinguished: '+\
-        #                        'timesteps too sparse.')
-
-        if roots == 'rotate':
-            point = start+t*vector
-            print '\nTrouble sorting roots:\nu = %s\n' % point
-            control_var = 'rotate'
-            break
-
-        else:
-            b = braiding_action(last_roots,roots)
-            
-            if b != 'I' and b != 'rotate':
-                G.append(b)
-            elif b == 'rotate':
-                control_var = 'rotate'
-                break
-
-            min_diff_rts = min(min_diff_rts,minimum_diff_rts(roots))
-            max_diff_cons = max(max_diff_cons,cons)
-            last_roots = roots
+        if is_degenerate(roots):
+            print 'Last roots: '+str(last_roots)
+            print 'Current roots:'+str(roots)
+            print 'Path data:'+str(start)+' to '+str(end)
+            print 'Location'+str(start+t*vector)
+            raise ValueError('Roots not being properly distinguished: '+\
+                               'timesteps too sparse.')
+        b = braiding_action(last_roots,roots)
+        if b!='I':
+            G.append(b)
+        min_diff_rts = min(min_diff_rts,minimum_diff_rts(roots))
+        max_diff_cons = max(max_diff_cons,cons)
+        last_roots = roots
         
     #if difference between roots smaller than timesteps, alert
     if min_diff_rts < max_diff_cons:
         print 'Timesteps too large to be reliable'
                     
-    if control_var == 'fine':
-        return [G,last_roots]
-
-    elif control_var == 'rotate':
-        return 'rotate'
+    return [G,last_roots]
 
 
 
@@ -1013,7 +935,7 @@ if __name__=="__main__":
     
     #Seiberg-Witten
     rot=np.exp(np.pi*1j/3.0 * 0.0)
-    xr=np.exp(np.pi*1j/3.0 * 0.0)
+    xr=np.exp(np.pi*1j/3.0)
     wmodel=WeierstrassProto(np.array([-1.0/3*rot**2,0,1.0/4.0])/xr**2,
                             np.array([-2.0/27*rot**3,0,1.0/12*rot,0])/xr**3)
                             
@@ -1029,7 +951,7 @@ if __name__=="__main__":
     #wmodel=WeierstrassProto([-1.0],[rot**2*1.0,0,0])
     dnum=0
     
-    # animate_roots_and_angles_path(wmodel,dnum,2,3,\
+    #animate_roots_and_angles_path(wmodel,dnum,2,3,\
     #                              timesteps=5000,steps=120,\
     #                              path=None,breaks=None)
 
@@ -1044,5 +966,9 @@ if __name__=="__main__":
     print(str(mon2))
 
     from elliptic_fibration import monodromy_eigencharge as ME
+
+    print wmodel.get_f()
+    print wmodel.get_g()
+
     print ME(mon1)
     print ME(mon2)

mose/elliptic_fibration.py
@@ -11,6 +11,7 @@ import weierstrass as wss
 
 from cmath import pi, exp, phase, sqrt
 from scipy.integrate import quad as n_int
+from numpy import linalg as LA
 
 #from sympy import Poly
 from branch import BranchPoint
@@ -93,7 +94,7 @@ class EllipticFibration:
                 """.format(
                     i,
                     branch_point_loci[i],
-                    branch_point_monodromies[i],
+                    numpy.array(branch_point_monodromies[i]).tolist(),
                     branch_point_charges[i],
                     bp_identifiers[i],
                 )
@@ -154,9 +155,53 @@ class EllipticFibration:
             + (g2_p(z) * g3_p(z) + 3 * g3(z) * g2_p_p(z) \
             - 2 * g2(z) * g3_p_p(z)) \
             / (3 * g3(z) * g2_p(z) - 2 * g2(z) * g3_p(z))
-
-
+        
         return [[0, 1], [M10(z), M11(z)]]
+
+
+        ### Attempt to use polyval, to make evaluation faster, 
+        ### needs some extra work.
+        ###
+        # print "TYPE: %s" % type((\
+        #         -18 * (g2 ** 2) * (g2_p ** 2) * g3_p \
+        #         + 3 * g2 * (7 * g3 * (g2_p ** 3) \
+        #         + 40 * (g3_p ** 3)) \
+        #         + (g2 ** 3) * (-8 * g3_p * g2_p_p \
+        #         + 8 * g2_p * g3_p_p) \
+        #         -108 * g3 \
+        #         * (-2 * g3 * g3_p * g2_p_p \
+        #         + g2_p \
+        #         * ((g3_p ** 2) + 2 * g3 * g3_p_p)) \
+        #         ) \
+        #         / (16 * ((g2 ** 3) -27 * (g3 ** 2)) \
+        #         * (-3 * g3 * g2_p + 2 * g2 * g3_p)\
+        #         ))
+
+        # M10 = numpy.polyval(\
+        #         (\
+        #         -18 * (g2 ** 2) * (g2_p ** 2) * g3_p \
+        #         + 3 * g2 * (7 * g3 * (g2_p ** 3) \
+        #         + 40 * (g3_p ** 3)) \
+        #         + (g2 ** 3) * (-8 * g3_p * g2_p_p \
+        #         + 8 * g2_p * g3_p_p) \
+        #         -108 * g3 \
+        #         * (-2 * g3 * g3_p * g2_p_p \
+        #         + g2_p \
+        #         * ((g3_p ** 2) + 2 * g3 * g3_p_p)) \
+        #         ) \
+        #         / (16 * ((g2 ** 3) -27 * (g3 ** 2)) \
+        #         * (-3 * g3 * g2_p + 2 * g2 * g3_p)\
+        #         ), z)
+
+        # M11 = numpy.polyval(\
+        #     (-3 * (g2 ** 2) * g2_p + 54 * g3 * g3_p) \
+        #     / ((g2 ** 3) - (27 * g3 ** 2)) \
+        #     + (g2_p * g3_p + 3 * g3 * g2_p_p \
+        #     - 2 * g2 * g3_p_p) \
+        #     / (3 * g3 * g2_p - 2 * g2 * g3_p), z)
+
+        # print "PF matrix : %s" % [[0, 1], [M10, M11]]
+        # return [[0, 1], [M10, M11]]
 
 
 #### NOW SUPERSEDED BY WEIERSTRASS CLASS ITSELF
@@ -194,28 +239,46 @@ class EllipticFibration:
 
 
 def monodromy_eigencharge(monodromy):
-    # m = magnetic charge
-    # n = electric charge
-    # we work in conventions of Seiberg-Witten 2, with monodromy matrices
-    # acting from the LEFT (hence our matrices are TRANSPOSE os those in SW!), 
-    # and given by:
-    #       1 + 2 n m    &    - m^2       \\
-    #       4 n^2        &    1 - 2 n m
+    # # m = magnetic charge
+    # # n = electric charge
+    # # we work in conventions of Seiberg-Witten 2, with monodromy matrices
+    # # acting from the LEFT (hence our matrices are TRANSPOSE os those in SW!), 
+    # # and given by:
+    # #       1 + 2 n m    &    - m^2       \\
+    # #       4 n^2        &    1 - 2 n m
 
-    nm = (monodromy[0][0] - monodromy[1][1]) / 4.0
-    m_temp = math.sqrt(-1.0 * monodromy[0][1])
-    n_temp = 2 * math.sqrt(monodromy[1][0] / 4.0)
-    if nm != 0:
-        if nm > 0:
-            n = n_temp
-            m = m_temp
-        else:
-            n = -n_temp
-            m = m_temp
+    # # print "this is the monodromy matrix: \n%s" % monodromy
+    # # print "this is the monodromy matrix type: %s" % type(monodromy)
+
+    # nm = (monodromy[0,0] - monodromy[1,1]) / 4.0
+    # m_temp = math.sqrt(-1.0 * monodromy[0,1])
+    # n_temp = 2 * math.sqrt(monodromy[1,0] / 4.0)
+    # if nm != 0:
+    #     if nm > 0:
+    #         n = n_temp
+    #         m = m_temp
+    #     else:
+    #         n = -n_temp
+    #         m = m_temp
+    # else:
+    #     m = m_temp
+    #     n = n_temp
+    # return (int(m), int(n))
+
+    eigen_sys = LA.eig(monodromy)
+    eigen_val_0 = int(numpy.rint(eigen_sys[0][0]))
+    eigen_val_1 = int(numpy.rint(eigen_sys[0][1]))
+    min_0 = min(abs(numpy.array(eigen_sys[1][0])[0]))
+    eigen_vec_0 = (numpy.array(eigen_sys[1][0])[0] / min_0).astype(int).tolist()
+    min_1 = min(abs(numpy.array(eigen_sys[1][1])[0]))
+    eigen_vec_1 = (numpy.array(eigen_sys[1][1])[0] / min_1).astype(int).tolist()
+    n_eigen_vec_1 = (-1 * numpy.array(eigen_sys[1][1])[0] / min_1).astype(int).tolist()
+
+    if eigen_vec_0 == eigen_vec_1 or eigen_vec_0 == n_eigen_vec_1:
+        return eigen_vec_0
     else:
-        m = m_temp
-        n = n_temp
-    return (int(m), int(n))
+        raise ValueError('Cannot compute monodromy eigenvector for the matrix\
+            \n'+str(monodromy))
 
 
 # def positive_period(n, charge, w_model, fibration): 