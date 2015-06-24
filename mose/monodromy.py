ccc83552ed1eadf276a30429a98775b23fe381e2
plonghi <pietro.longhi@gmail.com>
2015-06-24 17:38:50 -0400

    added N=2 star configuration file

--- mose/monodromy.py
+++ mose/monodromy.py
@@ -19,35 +19,35 @@ def charge_monodromy(charge, branch_point, direction):
 		return map(int, map(round, np.dot(charge, monodromy_matrix)))
 
 
-### !!!!	 UPDATE THE FOLLOWING 	!!!!
-###
 def flavor_charge_monodromy(gauge_charge, flavor_charge, branch_point, 
 															direction, dsz):
 	
-	### The monodromy matrix of a branch point with 
-	### gauge charge [p, q] and flavor charge [0,...,1,...,0]
-	### is given in blocks:
-	###	
-	### 	M 	|	A 
-	###	   -----------
-	###		0	|	1
-	###
-	### where M is th gauge charge monodromy, A is a 2 x N_f matrix
-	### witht eh following entries: 
-	###
-	###		0	0	... 	0	-q 	0	...
-	###		0	0	... 	0	p 	0	...
-	###
-	### in such a way that the (p, q, 0, ..., 1,... 0) charge of a branch
-	### point is a left eigenevctor for the monodromy.
-	### So the monodromy of a certain vector (m, n, 0, ... , 1, ..., 0) 
-	### will be (m, n) -> (m, n).M while 
-	### (0, ..., 1, ..., 0) --> (0, ..., 1, ..., <(m,n), (p,q)> ,..., 0)
-	### 
-	### Recalling that by default we assign a certain flavor charge to a 
-	### certain branch point, which is of the form:
-	### [0,...,1,...,0]
-	### we can express this in the following manner
+	"""
+	The monodromy matrix of a branch point with 
+	gauge charge [p, q] and flavor charge [0,...,1,...,0]
+	is given in blocks:
+		
+			M 	|	A 
+		   -----------
+			0	|	1
+	
+	where M is th gauge charge monodromy, A is a 2 x N_f matrix
+	witht eh following entries: 
+	
+			0	0	... 	0	-q 	0	...
+			0	0	... 	0	p 	0	...
+	
+	in such a way that the (p, q, 0, ..., 1,... 0) charge of a branch
+	point is a left eigenevctor for the monodromy.
+	So the monodromy of a certain vector (m, n, 0, ... , 1, ..., 0) 
+	will be (m, n) -> (m, n).M while 
+	(0, ..., 1, ..., 0) --> (0, ..., 1, ..., <(m,n), (p,q)> ,..., 0)
+	
+	Recalling that by default we assign a certain flavor charge to a 
+	certain branch point, which is of the form:
+	[0,...,1,...,0]
+	we can express this in the following manner.
+	"""
 
 	bp_gauge_charge = np.array(branch_point.gauge_charge)
 	bp_flavor_charge = np.array(branch_point.flavor_charge)
