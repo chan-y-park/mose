9541632da8e150de9921cde8c189b0bdf7930a12
plonghi <pietro.longhi@gmail.com>
2015-06-25 12:35:00 -0400

    fixes to multiple network plotting

--- mose/__init__.py
+++ mose/__init__.py
@@ -5,8 +5,10 @@ if platform.system() == 'Linux':
     try:
         os.environ['DISPLAY']
         matplotlib.use('TkAgg')
+        print('Use TkAgg backend for matplotlib.')
     except KeyError:
         matplotlib.use('Agg') 
+        print('Use Agg backend for matplotlib.')
 else:
     print('Use default backend defined in matplotlibrc: '
           '{}'.format(matplotlib.rcParams['backend']))
